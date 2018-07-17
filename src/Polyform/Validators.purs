d2e3bc90caa8f1e54f463b48706bb4b4a4a4390e
Tomasz Rybarczyk <paluho@gmail.com>
2018-07-17 20:35:31 +0200

    Cleanup module structure

--- src/Polyform/Validators.purs
+++ src/Polyform/Validators.purs
@@ -8,10 +8,21 @@ import Prelude
 
 import Data.Array (singleton)
 import Data.Variant (Variant)
-import Polyform.Validation (Validation, V(..))
+import Polyform.Validation (V(..), Validation, hoistFnMV, runValidation)
 
 type Errors e = Array (Variant e)
 type Validator m e a = Validation m (Errors e) a
 
 fail :: forall e a. Variant e -> V (Errors e) a
 fail e = Invalid $ singleton e
+
+check
+  :: forall e m a
+   . Monoid e
+  => Monad m => (a -> e) -> Validation m e a Boolean -> Validation m e a a
+check msg pred = hoistFnMV $ \v -> do
+  b <- runValidation pred v
+  pure $ case b of
+    Valid e true -> Valid e v
+    Valid e false -> Invalid $ (msg v) <> e
+    Invalid e -> Invalid e
