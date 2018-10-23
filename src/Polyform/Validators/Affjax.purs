01d24d767470d00181deff03447dc1bca612a254
Tomasz Rybarczyk <paluho@gmail.com>
2018-10-24 00:04:53 +0200

    Upgrade to affjax 7.0.0

--- src/Polyform/Validators/Affjax.purs
+++ src/Polyform/Validators/Affjax.purs
@@ -2,81 +2,93 @@ module Polyform.Validators.Affjax where
 
 import Prelude
 
+import Affjax (Response, ResponseFormatError, request, Request) as Affjax
+import Affjax.StatusCode (StatusCode(..))
 import Control.Monad.Error.Class (catchError)
-import Data.Argonaut (Json, jsonParser)
+import Data.Argonaut (Json)
 import Data.Array (singleton)
 import Data.Either (Either(..))
 import Data.Functor.Variant (SProxy(..))
-import Data.Validation.Semigroup (invalid)
-import Data.Variant (Variant, inj)
+import Data.Validation.Semigroup (V, invalid)
+import Data.Variant (inj)
 import Effect.Aff (Aff)
 import Foreign.Object (Object)
-import Network.HTTP.Affjax (AffjaxRequest, AffjaxResponse)
-import Network.HTTP.Affjax (affjax) as Affjax
-import Network.HTTP.Affjax.Response (Response, json, string)
-import Network.HTTP.StatusCode (StatusCode(..))
-import Polyform.Validator (Validator, hoistFnMV, hoistFnV)
+import Polyform.Validator (hoistFn, hoistFnMV, hoistFnV)
+import Polyform.Validators (Validator)
 import Polyform.Validators.Json (JsError, object)
+import Record (set)
+import Type.Row (type (+))
 
-type HttpErrorRow (err :: # Type) = (wrongHttpStatus :: StatusCode | err)
-type AffjaxErrorRow (err :: # Type) = (remoteError :: String | err)
-type JsonErrorRow (err :: # Type) = (parsingError :: String | err)
+type HttpError (err :: # Type) = (wrongHttpStatus :: StatusCode | err)
+type AffjaxError (err :: # Type) = (remoteError :: String | err)
+type ResponseFormatError (err :: # Type) =
+  (responseFormatError :: Affjax.Response Affjax.ResponseFormatError | err)
+
+valid :: forall e a. Semigroup e => a -> V e a
+valid = pure
 
 affjax
   :: forall a err
-   . (Response a)
-  -> Validator
+   . Validator
       Aff
-      (Array (Variant (AffjaxErrorRow err)))
-      AffjaxRequest
-      (AffjaxResponse a)
-affjax res = hoistFnMV $ \req → do
-  (pure <$> Affjax.affjax res req) `catchError` handler where
-    handler e = pure (invalid $ singleton $ (inj (SProxy :: SProxy "remoteError") $ show e))
+      (AffjaxError + ResponseFormatError + err)
+      (Affjax.Request a)
+      (Affjax.Response a)
+affjax = hoistFnMV $ \req -> do
+  (handleResponse <$> Affjax.request req) `catchError` handleError
+  where
+    handleResponse response = case response.body of
+      Left err ->
+        let
+          response' = set (SProxy :: SProxy "body") err response
+        in
+          invalid $ singleton $ (inj (SProxy :: SProxy "responseFormatError") response')
+      Right a ->
+        valid $ set (SProxy :: SProxy "body") a response
+    handleError e = pure (invalid $ singleton $ (inj (SProxy :: SProxy "remoteError") $ show e))
 
 status
   :: forall m err res
    . Monad m
   => (StatusCode -> Boolean)
   -> Validator m
-      (Array (Variant (HttpErrorRow err)))
-      (AffjaxResponse res)
+      (HttpError + err)
+      (Affjax.Response res)
+      (Affjax.Response res)
+status isCorrect = hoistFnV checkStatus
+  where
+    checkStatus response =
+      if isCorrect response.status then
+        pure response
+      else
+        invalid $ singleton $ (inj (SProxy :: SProxy "wrongHttpStatus") response.status)
+
+body
+  :: forall m err res
+   . Monad m
+  => Validator m
+      (HttpError + err)
+      (Affjax.Response res)
       res
-status isCorrect = hoistFnV checkStatus where
-  checkStatus response =
-    if isCorrect response.status then
-      pure response.response
-    else
-      invalid $ singleton $ (inj (SProxy :: SProxy "wrongHttpStatus") response.status)
+body = hoistFn _.body
 
 isStatusOK :: StatusCode -> Boolean
 isStatusOK (StatusCode n) = (n == 200)
 
-jsonFromRequest
+json
   :: forall err
    . Validator
       Aff
-      (Array (Variant(HttpErrorRow (AffjaxErrorRow (JsError err)))))
-      AffjaxRequest
+      (HttpError + AffjaxError + JsError + ResponseFormatError + err)
+      (Affjax.Request Json)
       (Object Json)
-jsonFromRequest = object <<< status isStatusOK <<< affjax json
-
-valJson
-  :: forall m err
-   . Monad m
-  => Validator m
-      (Array (Variant (JsonErrorRow err)))
-      String
-      Json
-valJson = hoistFnV \response -> case jsonParser response of
-  Right js -> pure js
-  Left error -> invalid  $ singleton (inj (SProxy :: SProxy "parsingError") error)
+json = object <<< body <<< status isStatusOK <<< affjax
 
 affjaxJson
   :: forall errs
    . Validator
       Aff
-      (Array (Variant (HttpErrorRow(AffjaxErrorRow (JsonErrorRow errs)))))
-      AffjaxRequest
+      (HttpError + AffjaxError + ResponseFormatError + errs)
+      (Affjax.Request Json)
       Json
-affjaxJson = valJson <<< status isStatusOK <<< affjax string
+affjaxJson = body <<< status isStatusOK <<< affjax
