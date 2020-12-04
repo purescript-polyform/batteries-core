4d4933f4e0d70cf5777d76d315f4d12718a0612c
Tomasz Rybarczyk <paluho@gmail.com>
2020-12-04 20:15:39 +0100

    Add enum based validators / duals

---
+++ src/Polyform/Batteries/Generic/Enum.purs
@@ -0,0 +1,34 @@
+module Polyform.Batteries.Generic.Enum where
+
+import Prelude
+import Data.Enum (class BoundedEnum, fromEnum, toEnum)
+import Data.Maybe (Maybe)
+import Polyform.Batteries (Validator, Dual)
+import Polyform.Batteries (error) as Batteries
+import Polyform.Dual (dual) as Dual
+import Polyform.Validator (liftFnMaybe) as Validator
+import Type.Prelude (SProxy(..))
+import Type.Proxy (Proxy(..))
+import Type.Row (type (+))
+
+_invalidEnumIndex = SProxy ∷ SProxy "invalidEnumIndex"
+
+type InvalidEnumIndex e
+  = ( invalidEnumIndex ∷ Int | e )
+
+-- | We use a `Proxy` value here to help the compiler choose
+-- | the output enum type.
+-- | If the output type is clear in your given context you can
+-- | use `validator'` and skip this `Proxy a` altogether.
+validator ∷ ∀ a err m. Applicative m ⇒ BoundedEnum a ⇒ Proxy a → Validator m (InvalidEnumIndex + err) Int a
+validator _ = Validator.liftFnMaybe (Batteries.error _invalidEnumIndex) (toEnum ∷ Int → Maybe a)
+
+-- | If you have clearly typed output passing in a `Proxy a` is redundant.
+validator' ∷ ∀ a err m. Applicative m ⇒ BoundedEnum a ⇒ Validator m (InvalidEnumIndex + err) Int a
+validator' = validator (Proxy ∷ Proxy a)
+
+dual ∷ ∀ a err m. Applicative m ⇒ BoundedEnum a ⇒ Proxy a → Dual m (InvalidEnumIndex + err) Int a
+dual p = Dual.dual (validator p) (pure <<< fromEnum)
+
+dual' ∷ ∀ a err m. Applicative m ⇒ BoundedEnum a ⇒ Dual m (InvalidEnumIndex + err) Int a
+dual' = dual (Proxy ∷ Proxy a)
--- src/Polyform/Batteries/Int.purs
+++ src/Polyform/Batteries/Int.purs
@@ -13,8 +13,8 @@ _intExpected = SProxy ∷ SProxy "intExpected"
 type IntExpected e
   = ( intExpected ∷ String | e )
 
-validator ∷ ∀ e m. Monad m ⇒ Batteries.Validator m (IntExpected + e) String Int
+validator ∷ ∀ e m. Applicative m ⇒ Batteries.Validator m (IntExpected + e) String Int
 validator = Validator.liftFnMaybe (Batteries.error _intExpected) Int.fromString
 
-dual ∷ ∀ e m. Monad m ⇒ Batteries.Dual m (IntExpected + e) String Int
+dual ∷ ∀ e m. Applicative m ⇒ Batteries.Dual m (IntExpected + e) String Int
 dual = Dual.dual validator (pure <<< show)
