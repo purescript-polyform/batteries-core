9b5697e8a1cf7bea6af4beec37cebe65ac09f363
Tomasz Rybarczyk <paluho@gmail.com>
2021-05-03 00:20:08 +0200

    Add msg helpers

--- src/Polyform/Batteries.purs
+++ src/Polyform/Batteries.purs
@@ -4,6 +4,8 @@ module Polyform.Batteries
   , Errors(..)
   , invalid
   , Msg
+  , msg
+  , msg'
   , Validator
   ) where
 
@@ -29,6 +31,21 @@ import Type.Prelude (class IsSymbol, SProxy)
 type Msg msg
   = { msg ∷ Data.Lazy String, info ∷ Variant msg }
 
+msg ∷ ∀ info l msg_ msg. IsSymbol l ⇒ Row.Cons l info msg_ msg ⇒ Data.Lazy String → SProxy l → info → Msg msg
+msg m l i = { msg: m, info: Variant.inj l i }
+
+msg' ::
+  ∀ t6 t7 t8 t9.
+  IsSymbol t8 ⇒
+  Row.Cons t8 t9 t7 t6 ⇒
+  String →
+  SProxy t8 →
+  t9 →
+  { info ∷ Variant t6
+  , msg ∷ Data.Lazy String
+  }
+msg' m = msg (defer \_ → m)
+
 -- | Do we want to migrate to this kind of error repr
 --   = Array ({ msg ∷ String, info ∷ Variant errs })
 type Errors err
@@ -42,7 +59,7 @@ type Dual m errs i o
 
 -- | Handy shortcuts to quickly build an error or the whole failure result
 error ∷ ∀ e errs l t. Row.Cons l e t errs ⇒ IsSymbol l ⇒ SProxy l → (e → String) → e → Errors errs
-error l msg = Array.singleton <<< \e → { msg: defer \_ → msg e, info: Variant.inj l e }
+error l prt = Array.singleton <<< \e → msg (defer \_ → prt e) l e
 
 invalid ∷ ∀ e errs l o t. Row.Cons l e t errs ⇒ IsSymbol l ⇒ SProxy l → (e → String) → e → V (Errors errs) o
-invalid l msg = Validation.invalid <<< error l msg
+invalid l prt = Validation.invalid <<< error l prt
